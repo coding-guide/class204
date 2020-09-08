4b6610841190c41e4f24ac73ab86b257eef45a8a
sunrye <srkim8744@korea.kr>
2020-09-08 15:27:48 +0900

    0908_3

--- src/exercise/chap06/Subway.java
+++ src/exercise/chap06/Subway.java
@@ -1,5 +1,23 @@
 package exercise.chap06;
 
 public class Subway {
+	public String lineNumber;
+	public int passengerCount;
+	public int money;
+	
+	public Subway(String lineNumber) {
+		this.lineNumber = lineNumber;
+	}
+	
+	public void pickUpPerson(int money) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Subway [lineNumber=" + lineNumber + ", passengerCount=" + passengerCount + ", money=" + money + "]";
+	}
+	
+	
 
 }
